6edef72ac37a3e84b17ecd357aec66bc57eda238
arrtxxm <70713621+arrtxxm@users.noreply.github.com>
2020-11-10 17:56:38 +0300

    Update lab1 Pashayan.cpp
    Проверка данных введенных пользователем, процедуры создания новых труб и КС

--- lab1 Pashayan/lab1 Pashayan.cpp
+++ lab1 Pashayan/lab1 Pashayan.cpp
@@ -6,8 +6,8 @@
 #include < fstream >
 using namespace std;
 
-
-struct PIPE
+	
+struct Pipe
 {
 	int id = 1;
 	double Length = 0.0;
@@ -18,15 +18,65 @@ struct PIPE
 struct CS
 {
 	int id = 1;
-	string name = "";
-	int shop = 0;
-	int shopwork = 0;
-	int efficiency = 0;
+	string Name = "";
+	int Shop = 0;
+	int WorkShop = 0;
+	int Efficiency = 0;
 };
 
-int main()
+template <typename T>
+T GetCorrectNumber(T min, T max)
 {
-	PIPE p;
+	T x;
+	cout << "Введите значение от " << min << " до " << max << ":";
+	cin >> x;
+	while (cin.fail() || x<min || x>max)
+	{
+		cin.clear();
+		cin.ignore(1000000, '\n');
+		cout << "Введите значение от " << min << " до " << max << ":";
+		cin >> x;
+	}
+	return x;
+}
+Pipe CreateNewPipe()
+{
+	Pipe newPipe;
+	cout << "Введите характеристики трубы:" << endl;
+	newPipe.id = 0;
+	newPipe.Repair = false;
+	cout << "Введите длину трубы в метрах" << endl;
+	newPipe.Length = GetCorrectNumber(0.0, 10000000.0);
+	cout << "Введите диаметр трубы в миллиметрах (мм) " << endl;
+	newPipe.Diameter = GetCorrectNumber(0, 999999999);
+	return newPipe;
+}
+CS CreatNewCS()
+{
+	CS newCS;
+	cout << "\nСчитайте данные для компрессарных станций: " << endl;
+	newCS.id = 1;
+	cout << "Введите имя компрессорной станции: ";
+	cin.get();
+	getline(cin, newCS.Name);
+	cout << "Введите количество цехов " << endl;
+	newCS.Shop = GetCorrectNumber(0, 999999999);
+	do
+	{
+		cout << "Введите количество цехов в работе " << endl;
+		newCS.WorkShop = GetCorrectNumber(0, 999999999);
+	} while (newCS.Shop < newCS.WorkShop); // проверка количество рбочих заводов между общим количеством заводов
+	cout << "Введите эффективность трубы (1-10) " << endl;
+	newCS.Efficiency = GetCorrectNumber(1, 10);
+	return newCS;
+}
+
+
+
+
+	int main()
+{
+	Pipe p;
 	CS c;
 }
 
